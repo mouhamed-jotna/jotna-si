f40a4cec063f571b72446449d2e31828f46ebf9d
mouhamed-jotna <mn781934410@gmail.com>
2025-09-02 00:15:42 +0000

    Create config.js
    Ajout de fichiers du site

---
+++ config.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyC76SfB7uQLLBdnKKcJ0rp15ZnTVcN2nTo",
+  authDomain: "jotna-si-express.firebaseapp.com",
+  projectId: "jotna-si-express",
+  storageBucket: "jotna-si-express.firebasestorage.app",
+  messagingSenderId: "35643086637",
+  appId: "1:35643086637:web:fad2be222dc6a12d00e5a4",
+  measurementId: "G-K7LZ7WM925"
